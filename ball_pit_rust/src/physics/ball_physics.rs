7c57d7386c2286b8250fc36c10987dd50f9f9551
maxwell3025 <44588341+maxwell3025@users.noreply.github.com>
2021-02-03 21:06:54 -0500

    created sectoring system
    created sectoring system
    created skeleton for interaction code in ball physics

--- ball_pit_rust/src/physics/ball_physics.rs
+++ ball_pit_rust/src/physics/ball_physics.rs
@@ -1,16 +1,23 @@
+ use std::collections::HashMap;
+use nalgebra::Vector2;
+
 use super::ball::Ball;
 
 pub struct BallPhysics{
-	balls: Vec<Ball>
+	balls: Vec<Ball>,
+	sectors: HashMap<Vector2<i32>, Vec<u32>>,
 }
 impl BallPhysics{
 	//constructor
 	pub fn new() -> BallPhysics{
 		let balls = Vec::new();
-		BallPhysics{balls}
+		let sectors = HashMap::new();
+		BallPhysics{balls, sectors}
 	}
 
 	pub fn update(&mut self, dt: f32){
-		
+		//add balls into sectors
+		//check for connections
+		//apply forces
 	}
 }
